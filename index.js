6333d0002726e1e6a60bb67f2aa177cd9eee25dc
José Andrés Ceballos Vadillo <andres@ceballos.dev>
2021-05-23 20:37:36 -0500

    add modal verbs test

--- index.js
+++ index.js
@@ -2,6 +2,14 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const removeDiacritics = require('diacritics').remove;
+
+const verbos = {
+  Konnen : {
+    ich : "musse",
+    du : "musst"
+  }  
+}
 
 const restService = express();
 
@@ -13,20 +21,21 @@ restService.use(
 
 restService.use(bodyParser.json());
 
-restService.post("/echo", function(req, res) {
-  var speech =
-    req.body.queryResult &&
-    req.body.queryResult.parameters &&
-    req.body.queryResult.parameters.echoText
-      ? req.body.queryResult.parameters.echoText
-      : "Seems like some problem. Speak again."+req.body;
+restService.post("/webhook", function(req, res) {
+  let speech = req.body.queryResult && 
+               req.body.queryResult.parameters               
+                ? selectAction(req)
+                : "Existe un problema. Repite por favor.";
+
+
+
   return res.json({
 
   "fulfillmentText": speech,
   "fulfillmentMessages": [
     {
       "text": {
-        "text": [speech]
+        "text": speech
       }
     }
   ],
@@ -36,6 +45,23 @@ restService.post("/echo", function(req, res) {
   });
 });
 
+function selectAction(req){
+  const action = req.body.queryResult.parameters;
+  if (action.verbosmodales){
+    return verboModal(req);
+  } 
+
+  return "queonda";
+}
+
+function verboModal(req){
+  const verbo = removeDiacritics(req.body.queryResult.parameters.verbosmodales);
+  if (req.body.queryResult.parameters.sujeto){
+    const sujeto = req.body.queryResult.parameters.sujeto;
+    return [verbos[verbo][sujeto]];
+  }
+  return Object.values(verbos[verbo]);
+}
 
 restService.listen(process.env.PORT || 8000, function() {
   console.log("Server up and listening");
